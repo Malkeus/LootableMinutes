6998c215ea98ee14d299455a9ab475dc4f31dcf0
Malkeus <mst3ktoo@gmail.com>
2018-12-14 01:27:39 -0600

    Updated
    Temporal Net can be toggled to recharge while slowing or drain while speeding. Off's still there too, I assume.

--- main.lua
+++ main.lua
@@ -3,13 +3,41 @@ local MOD = {}
 mods["LootableMinutes"] = MOD
 
 function MOD.on_turn_passed()
-	if (TimenetCast == false) then return end
-	local item = GetInvItem("time_net_active")
+	local item = nil
+	if (TimenetCast == false) then
+		if (TimenetRelease == false) then
+			return
+		else
+			item = GetInvItem("time_net_release")
+			if (item == nil) then
+				TimenetRelease = false
+				game.add_msg("You have misplaced your Temporal Net but have not folded it, you will have to refold and recast it to capture more Time.")
+				return
+			end
+			if (item.charges < 20) then
+				Timenet = player:i_add(item("time_net",1))
+				Timenet.charges = item.charges
+				TimenetCast = false
+				TimenetRelease = false
+				player:i_rem(item)
+				player:set_value("TimenetCast", "false")
+				player:set_value("TimenetRelease", "false")
+				game.add_msg("The net folds itself up, you have spent all of your spare Time.")
+			else
+				player:mod_moves(20)
+				item.charges = item.charges - 50
+			end
+		end
+		return
+	end
+			
+	item = GetInvItem("time_net_capture")
 	if (item == nil) then
 		TimenetCast = false
 		game.add_msg("You have misplaced your Temporal Net but have not folded it, you will have to refold and recast it to capture more Time.")
+		return
 	end
-	if (item.charges >= 9500) then
+	if (item.charges >= 19500) then
 		player:i_add(item("minute",1))
 		item.charges = 0
 		game.add_msg("A genuine Minute congeals from your Temporal Net, it will be invaluable if you happen to need more Time in a pinch.")
@@ -33,14 +61,14 @@ function GetInvItem(itemid)
 	return item
 end
 
-
-
 function MOD.on_new_player_created()
 	if (player:get_value("TimeNet") == "true") then
 	else
 		player:i_add(item("time_net",1))
 		player:set_value("TimeNet", "true")
 		game.add_msg("A mysterious net appears before you, warping strangely in ways that seem impossible")
+		player:set_value("TimenetCast", "false")
+		player:set_value("TimenetRelease", "false")
 	end
 end
 
@@ -60,9 +88,16 @@ function MOD.on_savegame_loaded()
 		else
 			TimenetCast = false
 		end
+		if (player:get_value("TimenetRelease") == "true") then
+			TimenetRelease = true
+		else
+			TimenetRelease = false
+		end
 	else
 		player:i_add(item("time_net",1))
 		player:set_value("TimeNet", "true")
 		game.add_msg("A mysterious net appears before you, warping strangely in ways that seem impossible")
+		player:set_value("TimenetCast", "false")
+		player:set_value("TimenetRelease", "false")
 	end
 end
--- preload.lua
+++ preload.lua
@@ -8,23 +8,46 @@ end
 
 Timenet = nil
 TimenetCast = false
+TimenetRelease = false
 
 function cast_timenet(item, active)
-	Timenet = player:i_add(item("time_net_active",1))
-	Timenet.charges = item.charges
-	TimenetCast = true
-	player:i_rem(item)
-	game.add_msg("You try to cast the net but it doesn't move, you feel sluggish.")
-	player:set_value("TimenetCast", "true")
+	local menu = game.create_uimenu()
+	local choice = -1
+	menu.title = "Which way do you cast your net?"
+	menu:addentry("Forward")
+	menu:addentry("Backwards")
+	menu:addentry("Cancel")
+	menu:query(true)
+	choice = menu.selected
+	
+	if (choice == 0) then
+		Timenet = player:i_add(item("time_net_capture",1))
+		Timenet.charges = item.charges
+		TimenetCast = true
+		player:i_rem(item)
+		game.add_msg("You cast the net ahead of you but it doesn't move, you feel sluggish.")
+		player:set_value("TimenetCast", "true")
+	elseif (choice == 1) then
+		Timenet = player:i_add(item("time_net_release",1))
+		Timenet.charges = item.charges
+		TimenetRelease = true
+		player:i_rem(item)
+		game.add_msg("You cast the net sdrawkcab and it begins to writhe, you feel faster")
+		player:set_value("TimenetRelease", "true")
+	elseif (choice == 2) then
+		return
+	end
 end
 
 function fold_timenet(item, active)
 	Timenet = player:i_add(item("time_net",1))
 	Timenet.charges = item.charges
 	TimenetCast = false
+	TimenetRelease = false
 	player:i_rem(item)
-	game.add_msg("You fold up the net and feel much faster.")
+	game.add_msg("You fold up the net and feel normal again.")
 	player:set_value("TimenetCast", "false")
+	player:set_value("TimenetRelease", "false")
 end
 
 game.register_iuse("IUSE_MINUTE", minute)
